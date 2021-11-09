e02d88dc8218cdc681a86a0fca0376096b3b504c
yared123yared <yaredyaya16@gmail.com>
2021-11-09 14:48:10 +0300

    commited changes

--- Repository/customerRepository.cs
+++ Repository/customerRepository.cs
@@ -34,8 +34,8 @@ namespace broker.Data
              .Include(e => e.Deals).ThenInclude(e=>e.Broker).ThenInclude(e=>e.User)
              
              
-            // .Include(e => e.Delivery).ThenInclude(e=>e.Broker).ThenInclude(e=>e.User)
-            //  .Include(e => e.Delivery).ThenInclude(e=>e.Broker).ThenInclude(e=>e.Category)
+            .Include(e => e.Delivery).ThenInclude(e=>e.Broker).ThenInclude(e=>e.User)
+             .Include(e => e.Delivery).ThenInclude(e=>e.Broker).ThenInclude(e=>e.Category)
               .Include(e => e.Delivery).ThenInclude(e=>e.Broker).ThenInclude(e=>e.Skills)
             
 // .Include(e => e.User
